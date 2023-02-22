27bd29d2a991d28e66c6385518201bea28e1dae4
Gsttv <gustvmorais@gmail.com>
2023-02-22 13:27:39 -0300

    Commit consertando o loop

--- Funcionarios.java
+++ Funcionarios.java
@@ -8,11 +8,11 @@ public class Funcionarios {
         System.out.println("Digite a quantidade de funcnionarios:");
         int qnt = sc.nextInt();
         List<Dados> funcionario = new ArrayList<>();
-        for (int i=0 ; i<qnt; i++){
-            System.out.println("Digite os dados do funcionario "+i);
+        for (int i = 0; i < qnt; i++) {
+            System.out.println("Digite os dados do funcionario " + i);
             System.out.println("ID");
             int id = sc.nextInt();
-            while (idExistente(funcionario,id)){
+            while (idExistente(funcionario, id)) {
                 System.out.println("ID EXISTENTE, DIGITE NOVAMENTE");
                 id = sc.nextInt();
             }
@@ -21,24 +21,27 @@ public class Funcionarios {
             String nome = sc.nextLine();
             System.out.println("SALARIO");
             double salario = sc.nextDouble();
-            Dados func = new Dados(id,nome,salario);
+            Dados func = new Dados(id, nome, salario);
             funcionario.add(func);
         }
         System.out.println("Digite o id do funcionario que receberá o aumento");
         int id_funcionario = sc.nextInt();
-        System.out.println("Digite a porcentagem de aumento");
-        double perc = sc.nextDouble();
-        Dados busca_id = funcionario.stream().filter(x -> x.getId()== id_funcionario).findFirst().orElse(null);
-        boolean a = false;
-        while (a==false){ //NÃO ESTA FUNCIONADO, LOOP INFINITO.
-            busca_id = funcionario.stream().filter(x -> x.getId()== id_funcionario).findFirst().orElse(null);
+
+        Dados busca_id = funcionario.stream().filter(x -> x.getId() == id_funcionario).findFirst().orElse(null);
+
+        while (busca_id == null) {
             if (busca_id != null) {
-                busca_id.aumento(perc);
-                a = true;
+                break;
             } else {
                 System.out.println("ID NÃO EXISTE, DIGITE UM ID VALIDO");
+                int id_funcionario2 = sc.nextInt();
+                busca_id = funcionario.stream().filter(x -> x.getId() == id_funcionario2).findFirst().orElse(null);
             }
         }
+        System.out.println("Digite a porcentagem de aumento");
+        double perc = sc.nextDouble();
+        busca_id.aumento(perc);
+
         System.out.println("LISTA DE FUNCIOANRIOS");
         for (Dados e : funcionario) {
             System.out.println(e);
@@ -46,8 +49,9 @@ public class Funcionarios {
 
         sc.close();
     }
-    public static boolean idExistente(List<Dados> lista, int id){
-        Dados teste = lista.stream().filter(x -> x.getId()== id).findFirst().orElse(null);
+
+    public static boolean idExistente(List<Dados> lista, int id) {
+        Dados teste = lista.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
         return teste != null;
 
     }
